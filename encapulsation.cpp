b45abb06ba69ba3b8863b8d6467299af2ffa1839
Sanket Singhal <sanketsinghal@192.168.1.5>
2024-12-05 05:12:35 +0530

    first commit

---
+++ encapulsation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Encapsulation: Means Binding of Data Members and Member function inside one class.
+
+class Product {
+    private:
+        string productName;
+        
+};
+
+
+
+int main() {
+
+}
