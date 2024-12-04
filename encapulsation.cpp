cde2c79d87175d1e4c85d8e1deea87c592af9a5d
Sanket Singhal <sanketsinghal1999@gmail.com>
2024-12-05 05:29:51 +0530

    Added "Encapsulated Code"

--- encapulsation.cpp
+++ encapulsation.cpp
@@ -3,14 +3,51 @@ using namespace std;
 
 // Encapsulation: Means Binding of Data Members and Member function inside one class.
 
-class Product {
-    private:
-        string productName;
-        
+class Product
+{
+private:
+    string productName;
+    int productPrice;
+
+public:
+    void setProductPrice(int price)
+    {
+        if (price > 0)
+        {
+            productPrice = price;
+        }
+        else
+        {
+            cout << "Invalid Price Entered" << endl;
+        }
+    }
+    void setProductName(string pName)
+    {
+        if (pName != "")
+        {
+            productName = pName;
+        }
+        else
+        {
+            cout << "Invalid Product Name Entered" << endl;
+        }
+    }
+    int getProductPrice()
+    {
+        return productPrice;
+    }
+    string getProductName()
+    {
+        return productName;
+    }
 };
 
-
-
-int main() {
-
+int main()
+{
+    Product p1;
+    p1.setProductName("Iphone");
+    p1.setProductPrice(100000);
+    cout << "Product Name: " << p1.getProductName() << endl;
+    cout << "Product Price: " << p1.getProductPrice() << endl;
+    return 0;
 }
